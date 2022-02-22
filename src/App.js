41aedf5328ec17af056f87cc1ba15308a6fb784b
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2022-02-22 16:13:38 -0500

    front end basics

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./components/screens/HomeScreen";
+
+function App() {
+  return (
+    <> 
+    
+      <Header />
+      <main className="py-3">
+          <Container>
+          ProShop
+          <HomeScreen/>
+          </Container>
+          
+      </main>
+      <Footer />
+     
+    </>
+  );
+}
+
+export default App;
